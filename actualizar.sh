446b7dbabbbe83d4b517cdbd6ce11ecded309cce
AgustinPrf <aguaguileramartin@gmail.com>
2022-11-07 18:46:36 +0100

    subiendo script

---
+++ actualizar.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Recuerda : Tu token es -> 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 " 
+git pull origin gh-pages
+
+echo "---------------------------------------------------------"
+echo "+++++ !Enhorabuena!! has actualizado la página de comandos linux"
+echo "---------------------------------------------------------"
